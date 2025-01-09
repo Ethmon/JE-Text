6be1c4231a5a3ad6b51ea897fd2d911ff34c60b0
Ethmon <Ethan6salu@gmail.com>
2025-01-09 15:04:16 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using JE;
+
+namespace JE_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
